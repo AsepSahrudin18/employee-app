16d8dfb9c9674af10caccf0f6ac9b8596cf35446
AsepSahrudin18 <asepsahrudin0399@gmail.com>
2024-05-24 22:59:44 +0700

    add fitur login & register user

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <h1>Employee Data</h1>
         <button class="btn btn-success mb-3" data-toggle="modal" data-target="#createEmployeeModal">Create New
             Employee</button>
